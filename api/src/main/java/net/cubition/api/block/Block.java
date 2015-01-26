53c9e69bb564954ea04fb9907d4774493d98f5d1
Xesau <ck-duinrand@zeelandnet.nl>
2015-01-26 21:48:26 +0100

    Bloxx

--- api/src/main/java/net/cubition/api/block/Block.java
+++ api/src/main/java/net/cubition/api/block/Block.java
@@ -3,10 +3,24 @@ package net.cubition.api.block;
 import net.cubition.api.world.Chunk;
 import net.cubition.api.world.Location;
 
-public class Block {
+public class Block
+{
 
+	/**
+	 * The chunk this block is in
+	 */
 	private Chunk chunk;
+	
+	/**
+	 * The coordinates of the block
+	 * relative to the chunk
+	 */
 	private int x, y, z;
+	
+	/**
+	 * The current state of the block
+	 */
+	private BlockState state;
 	
 	public Block (Location location)
 	{
@@ -16,19 +30,43 @@ public class Block {
 		this.z = (int) location.getRelativeZ ();
 	}
 	
+	/**
+	 * @return the location of this block
+	 */
 	public Location getLocation ()
 	{
 		return new Location (chunk.getWorld (), chunk.getStartX (), chunk.getStartY (), chunk.getStartZ ());
 	}
 	
+	/**
+	 * @return the type this block is
+	 */
 	public BlockType getType ()
 	{
 		return chunk.getBlockType (x, y, z);
 	}
 	
+	/**
+	 * Sets the type of this block
+	 * 
+	 * @param type the type
+	 */
 	public void setBlockType (BlockType type)
 	{
 		chunk.setBlockType (x, y, z, type);
+	}
+	
+	/**
+	 * @return the current block state
+	 */
+	public BlockState getBlockState ()
+	{
+		return state;
+	}
+	
+	public void setBlockData (BlockState state)
+	{
+		
 	}
 	
 }
---
+++ api/src/main/java/net/cubition/api/block/BlockState.java
@@ -0,0 +1,5 @@
+package net.cubition.api.block;
+
+public interface BlockState {
+
+}
