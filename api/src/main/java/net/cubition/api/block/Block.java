2523f7e679f2cf997e98629552b566f8cb62a531
Xesau <ck-duinrand@zeelandnet.nl>
2015-01-26 22:10:08 +0100

    API: Fix bad block class
    This fixes bug numero 1.

--- api/src/main/java/net/cubition/api/block/Block.java
+++ api/src/main/java/net/cubition/api/block/Block.java
@@ -3,7 +3,6 @@ package net.cubition.api.block;
 import net.cubition.api.world.Chunk;
 import net.cubition.api.world.Location;
 
-<<<<<<< HEAD
 public class Block
 {
 
@@ -69,29 +68,5 @@ public class Block
 	{
 		
 	}
-	
-=======
-public class Block {
-    private Chunk chunk;
-    private int x, y, z;
 
-    public Block(Location location) {
-        this.chunk = location.getChunk();
-        this.x = (int) location.getRelativeX();
-        this.y = (int) location.getRelativeY();
-        this.z = (int) location.getRelativeZ();
-    }
-
-    public Location getLocation() {
-        return new Location(chunk.getWorld(), x, y, z);
-    }
-
-    public BlockType getType() {
-        return chunk.getBlockType(x, y, z);
-    }
-
-    public void setBlockType(BlockType type) {
-        chunk.setBlockType(x, y, z, type);
-    }
->>>>>>> origin/master
 }
