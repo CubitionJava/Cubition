49ab4d8f23a2f9069798459e07e8dec47aeabb88
Xesau <aron2000@zeelandnet.nl>
2015-01-28 22:15:02 +0100

    API: Redundant JavaDocs removed
    BaseModManager already had JavaDocs inherited from ModManager

--- api/src/main/java/net/cubition/api/mod/BaseModManager.java
+++ api/src/main/java/net/cubition/api/mod/BaseModManager.java
@@ -6,24 +6,14 @@ import java.util.List;
 public class BaseModManager implements ModManager {
     private List<Mod> mods = new ArrayList<>();
 
-    /**
-     * Gets all loaded mods
-     *
-     * @return All loaded mods
-     */
     @Override
     public List<Mod> getMods() {
         return mods;
     }
 
-    /**
-     * Loads a mod
-     *
-     * @param mod The path to the mod's file
-     * @return null, for now
-     */
     @Override
     public Mod load(String mod) {
         return null;
     }
+
 }
