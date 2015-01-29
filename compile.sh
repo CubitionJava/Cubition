2a1980d6399acbdfa0202d9b4f51963bb4b9b4c5
James <jselby@jselby.net>
2015-01-29 12:12:50 +1100

    Global: Build javadoc folders for 'core'

--- compile.sh
+++ compile.sh
@@ -2,9 +2,6 @@
 # A very basic Cubition compile script for Mac + Linux
 # This expects Maven is available in the environment under 'mvn'.
 
-# Check for mvn existance
-type mvn >/dev/null 2>&1 || { echo >&2 "I couldn't find Maven in the environment via the command 'mvn'. Aborting..."; exit 1; }
-
 # Clean up environment
 echo Cleaning up...
 mvn clean
@@ -15,10 +12,12 @@ mkdir -p api/target/site/apidocs
 mkdir -p client/target/site/apidocs
 mkdir -p server/target/site/apidocs
 mkdir -p bootstrap/target/site/apidocs
+mkdir -p core/target/site/apidocs
 mkdir -p out/javadoc-api
 mkdir -p out/javadoc-client
 mkdir -p out/javadoc-server
 mkdir -p out/javadoc-bootstrap
+mkdir -p out/javadoc-core
 
 # Produce final product
 echo Running Maven...
