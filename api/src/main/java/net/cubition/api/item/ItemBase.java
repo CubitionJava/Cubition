b28b34026bc6d62708924893c5e5aaa456a14021
Xesau <aron2000@zeelandnet.nl>
2015-01-29 20:52:34 +0100

    API: Changed ItemBase to Interface. Added ItemStack

--- api/src/main/java/net/cubition/api/item/ItemBase.java
+++ api/src/main/java/net/cubition/api/item/ItemBase.java
@@ -1,5 +1,27 @@
 package net.cubition.api.item;
 
-public class ItemBase {
+public interface ItemBase {
 
+	/**
+	 * Returns whether the item should be destroyed on explosion
+	 * or on interaction with destroying-liquids
+	 * 
+	 * @return true or false
+	 */
+	public boolean destroyOnExplode ();
+	
+	/**
+	 * Returns the translatable name of this item
+	 * @return The translatable name
+	 */
+	public String getTranslatableName ();
+	
+	/**
+	 * Returns the translatable name of this item with a certain damage-value
+	 * 
+	 * @param damage The damage-value
+	 * @return The translatable name
+	 */
+	public String getTranslatableName (byte damage);
+	
 }
