cc99979effa7ca8c89fe32548bc24b93807769c3
Xesau <aron2000@zeelandnet.nl>
2015-01-30 20:33:24 +0100

    API: New resource (block/item/tool) interfaces!

---
+++ api/src/main/java/net/cubition/api/network/NetworkIDResource.java
@@ -0,0 +1,11 @@
+package net.cubition.api.network;
+
+public interface NetworkIDResource {
+	
+	/**
+     * Get the ID that will be sent over the Network when 
+     * @return the id
+     */
+    public String getNetworkID ();
+    
+}
