e3344ea81cec6653100242ebf342122157ec74be
Acharige <acharige@cubition.net>
2015-05-09 10:35:49 -0400

    Added ItemStoneShears
    Still a long way to go.

---
+++ minecraft/src/main/java/net/cubition/minecraft/item/ItemStoneHoe.java
@@ -0,0 +1,38 @@
+package net.cubition.minecraft.item;
+
+import net.cubition.api.API;
+import net.cubition.api.TranslatableResource;
+import net.cubition.api.item.ToolItem;
+import net.cubition.api.network.NetworkIDResource;
+import net.cubition.api.tool.ToolType;
+import net.cubition.minecraft.tool.ToolHoe;
+
+public class ItemStoneShovel extends ToolItem implements NetworkIDResource,
+		TranslatableResource {
+
+	@Override
+	public String getNetworkID() {
+		return "291";
+	}
+
+	@Override
+	public String getTranslatableName() {
+		return "item.hoeStone";
+	}
+
+	@Override
+	public String getTranslatableName(byte damage) {
+		return "item.hoeStone";
+	}
+
+	@Override
+	public int maxStackSize() {
+		return 1;
+	}
+
+	@Override
+	public ToolType getToolType() {
+		return API.getToolTypeRegister().get(ToolHoe.class.getName());
+	}
+
+}
