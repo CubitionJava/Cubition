7ac729f2b282e17ed9b0f5cefe6c1f4b82496ad2
nverdierDev <nayamichaelv@gmail.com>
2015-05-23 23:13:11 -0700

    Add libGDX!!! Gradle needed... http://gradle.org/downloads/

---
+++ client/core/src/com/nayaverdier/gdx/GDXGame.java
@@ -0,0 +1,23 @@
+package com.nayaverdier.gdx;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GDXGame extends ApplicationAdapter {
+	SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+}
---
+++ client/html/src/com/nayaverdier/gdx/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.nayaverdier.gdx.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.nayaverdier.gdx.GDXGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GDXGame();
+        }
+}
