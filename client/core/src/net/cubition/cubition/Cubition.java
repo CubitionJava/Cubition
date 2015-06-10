0ae5d22a2a7e5049ad2b4d15dc37c5c47ae12509
Xesau <Xesau@users.noreply.github.com>
2015-06-10 13:05:34 +0200

    Clear the screen at every render.

--- client/core/src/net/cubition/cubition/Cubition.java
+++ client/core/src/net/cubition/cubition/Cubition.java
@@ -16,6 +16,10 @@ public class Cubition extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		// Reset GL: Clear to black
+		Gdx.gl.glClearColor (0, 0, 0, 1);
+		Gdx.gl.glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 		// Render the current view, nothing special
 		// Nothing needs to be passed to .render(). Gtx is static public
 		view.render ();
