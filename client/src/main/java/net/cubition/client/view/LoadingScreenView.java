1a32d9d52377b745cdc4f3d64e6726994341e187
Xesau <Xesau@users.noreply.github.com>
2015-08-07 15:15:24 +0200

    Update LoadingScreenView.java

--- client/src/main/java/net/cubition/client/view/LoadingScreenView.java
+++ client/src/main/java/net/cubition/client/view/LoadingScreenView.java
@@ -4,23 +4,23 @@ import net.cubition.client.View;
 import de.matthiasmann.twl.utils.PNGDecoder;
 
 public class LoadingScreenView implements View {
-	
-@Override
-public void render () {
-InputStream in = new FileInputStream("logo.png");
-
-try {
-   PNGDecoder decoder = new PNGDecoder(in);
- 
-   System.out.println("width="+decoder.getWidth());
-   System.out.println("height="+decoder.getHeight());
- 
-   ByteBuffer buf = ByteBuffer.allocateDirect(4*decoder.getWidth()*decoder.getHeight());
-   decoder.decode(buf, decoder.getWidth()*4, Format.RGBA);
-   buf.flip();
-} finally {
-   in.close();
-}
-}
+		
+	@Override
+	public void render () {
+		InputStream in = new FileInputStream("logo.png");
+		
+		try {
+			PNGDecoder decoder = new PNGDecoder(in);
+			
+			System.out.println("width="+decoder.getWidth());
+			System.out.println("height="+decoder.getHeight());
+			
+			ByteBuffer buf = ByteBuffer.allocateDirect(4*decoder.getWidth()*decoder.getHeight());
+			decoder.decode(buf, decoder.getWidth()*4, Format.RGBA);
+			buf.flip();
+		} finally {
+			in.close();
+		}
+	}
 	
 }
